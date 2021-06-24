51fae51295447fca84432ba7374d07aa6e044473
owen, ya boy <oheichhorn19@coe.edu>
2021-06-24 16:59:56 -0500

    New longitude plots! GOOD!

---
+++ supplimentary_material/dicoms/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#imagej 6MV/1x3.dcm plotting3.ijm & 
+#imagej 6MV/3x3.dcm plotting3.ijm & 
+#imagej 6MV/3x1.dcm plotting3.ijm & 
+#imagej 6MV/2x2.dcm plotting3.ijm & 
+#imagej 6MV/2x4.dcm plotting3.ijm & 
+#imagej 6MV/4x2.dcm plotting3.ijm & 
+#imagej 6MV/5x5.dcm plotting3.ijm & 
+imagej 6MV/1x3.dcm plotting4.ijm & 
+imagej 6MV/3x3.dcm plotting4.ijm & 
+imagej 6MV/3x1.dcm plotting4.ijm & 
+imagej 6MV/2x2.dcm plotting4.ijm & 
+imagej 6MV/2x4.dcm plotting4.ijm & 
+imagej 6MV/4x2.dcm plotting4.ijm & 
+imagej 6MV/5x5.dcm plotting4.ijm 
+
---
+++ supplimentary_material/dicoms/script2.sh
@@ -0,0 +1,14 @@
+imagej 10MV/1x3.dcm plotting3.ijm & 
+imagej 10MV/3x3.dcm plotting3.ijm & 
+imagej 10MV/3x1.dcm plotting3.ijm & 
+imagej 10MV/4x2.dcm plotting3.ijm & 
+imagej 10MV/2x4.dcm plotting3.ijm & 
+imagej 10MV/2x2.dcm plotting3.ijm & 
+imagej 10MV/5x5.dcm plotting3.ijm & 
+imagej 10MV/1x3.dcm plotting4.ijm & 
+imagej 10MV/3x3.dcm plotting4.ijm & 
+imagej 10MV/3x1.dcm plotting4.ijm & 
+imagej 10MV/4x2.dcm plotting4.ijm & 
+imagej 10MV/2x4.dcm plotting4.ijm & 
+imagej 10MV/2x2.dcm plotting4.ijm & 
+imagej 10MV/5x5.dcm plotting4.ijm 
