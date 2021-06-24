2d137f1e15893f4dcf74a1d3b7227984d3521827
owen, ya boy <oheichhorn19@coe.edu>
2021-06-24 15:20:07 -0500

    Plotted Nathan's OSU data in figures/nn2DicomComp

---
+++ supplimentary_material/dicoms/plotting3.ijm
@@ -0,0 +1,4 @@
+setSlice(136);
+run("Rotate 90 Degrees Left");
+makeRectangle(0, 0, 202, 202);
+run("Plot Profile");
---
+++ supplimentary_material/dicoms/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#imagej 6MV/1x3.dcm plotting3.ijm & 
+#imagej 6MV/3x3.dcm plotting3.ijm & 
+#imagej 6MV/3x1.dcm plotting3.ijm & 
+#imagej 6MV/2x2.dcm plotting3.ijm & 
+#imagej 6MV/2x4.dcm plotting3.ijm & 
+#imagej 6MV/4x2.dcm plotting3.ijm & 
+#imagej 6MV/5x5.dcm plotting3.ijm & 
+imagej 10MV/1x3.dcm plotting3.ijm & 
+imagej 10MV/3x3.dcm plotting3.ijm & 
+imagej 10MV/3x1.dcm plotting3.ijm & 
+imagej 10MV/4x2.dcm plotting3.ijm & 
+imagej 10MV/2x4.dcm plotting3.ijm & 
+imagej 10MV/2x2.dcm plotting3.ijm & 
+imagej 10MV/5x5.dcm plotting3.ijm  
